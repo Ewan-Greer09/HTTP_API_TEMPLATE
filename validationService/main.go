0f3d6b31c1258bb2decbf5adbbc7bce9be4f3333
Ewan-Greer09 <egreer09@qub.ac.uk>
2023-01-30 21:03:07 +0000

    remove comments

--- validationService/main.go
+++ validationService/main.go
@@ -8,9 +8,6 @@ import (
 	"github.com/Ewan-Greer09/HTTP_API_TEMPLATE/validationService/server"
 )
 
-//TODO: Call the validation service once it is online
-//TODO: Call the Auth service once it is online
-//TODO: Move to a Database once it is online
 //TODO: Move to a Docker container once it is online
 //TODO: Move values to a config file once we have one
 
