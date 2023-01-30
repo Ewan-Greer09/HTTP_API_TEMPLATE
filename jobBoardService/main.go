6003128be7c6ceb6f50586d50d63201d2a3105b0
Ewan Greer <111976535+Ewan-Greer09@users.noreply.github.com>
2023-01-30 21:37:36 +0000

    Create server and handler valid serv (#2)

--- jobBoardService/main.go
+++ jobBoardService/main.go
@@ -8,18 +8,23 @@ import (
 	"github.com/Ewan-Greer09/HTTP_API_TEMPLATE/jobBoardService/server"
 )
 
-//TODO: Call the validation service once it is online
 //TODO: Call the Auth service once it is online
 //TODO: Move to a Database once it is online
 //TODO: Move to a Docker container once it is online
 //TODO: Move values to a config file once we have one
+//TODO: Add a logger once we have one
+
+const (
+	port       = "8080"
+	listenAddr = "localhost"
+)
 
 func main() {
 	startTime := time.Now()
 	timestamp := startTime.Format("2006-01-02 15:04:05.000000")
 	log.Println("Start: ", timestamp)
 
-	handler := handlers.NewHandler()
+	server := server.NewServer(handler, port, listenAddr)
 
 	server.StartServer(handler)
 
