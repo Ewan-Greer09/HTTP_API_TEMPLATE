879f61c07635b0969b05bdd41a93dc1e85cb5176
Ewan Greer <111976535+Ewan-Greer09@users.noreply.github.com>
2023-02-28 02:00:04 +0000

    Job board new (#8)
    * update job listing naming
    
    * add pr template

--- start.cmd
+++ start.cmd
@@ -1,2 +1,4 @@
 @echo off
-nodemon -e go -x "go run .\jobBoardService\main.go"
+nodemon -e go -x "go run .\jobboard\main.go"
+nodemon -e go -x "go run .\authserver\main.go"
+nodemon -e go -x "go run .\validationserver\main.go"
